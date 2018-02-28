66867accb08f8a94deb1b4c4357e486e2af056dc
Patrick Meiler <patrick@meiler.org>
2018-02-28 13:46:57 +0100

    New Iteration

--- server.js
+++ server.js
@@ -1,11 +1,36 @@
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
+const middleware = require("./middleware.js");
 
+var todos = [
+	{
+		id: 1,
+		description: "Water your plants!",
+		done: false
+	},
+	{
+		id: 2,
+		description: "Go pet your Animal",
+		done: false
+	},
+	{
+		id: 3,
+		description: "Do something for your Thesis!",
+		done: false	
+	}
+]
+
+app.use(middleware.logger);
 
 app.get("/",function(req,res){
 	res.send("We listen!");
 });
+
+app.get("/todo",function(req,res){
+	res.json(todos);
+})
+
 
 app.use(express.static(__dirname+"/public"));
 
