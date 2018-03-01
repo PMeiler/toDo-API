f88599466b1ecb166aef4ceffbd5bb5164e50729
Patrick Meiler <patrick@meiler.org>
2018-03-01 23:05:31 +0100

    Added DELETE

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 const middleware = require("./middleware.js");
 const bodyParser = require("body-parser");
+const _ = require("underscore");
 
 
 
@@ -48,13 +49,7 @@ app.get("/todo",function(req,res){
 //Ausgabe eines bestimmten ToDo Elementes
 app.get("/todo/:id",function(req,res){
 	var gId = parseInt(req.params.id,10);
-	var found;
-
-	todos.forEach(function(element){
-		if(gId === element.id){
-			found = element;
-		}
-	});
+	var found = _.findWhere(todos,{id:gId});
 
 	if(found){
 		res.json(found);
@@ -64,32 +59,40 @@ app.get("/todo/:id",function(req,res){
 	
 });
 
+
+//Füge ein neues Element der ToDo Liste bei!
 app.post("/todo/add", function(req,res){
 	
-	/*
-	var a = req.body;
-	console.log(a);
-	console.log(a.id);
-	console.log(a.description);
-	res.send(req.body);
-	*/
-	addToArray(req.body);
+	var body = req.body;
+
+	if(!_.isBoolean(body.done) || !_.isString(body.description) || body.description.trim().length === 0){
+		return res.status(400).send();
+	}
+	body.description = body.description.trim();
+
+	addToArray(_.pick(body,"description","done"));
 
 
 	function addToArray(element){
-		if(element.description == ""  || typeof element.description == "undefined"){
-			res.send("No Element was Provided!")
-		}else{
-
-			var newObject = {
-				element: todos.length,
-				description: element.description,
-				done: false
-			};
-			todos.push(newObject);
+			element.id = todos.length + 1;
+			todos.push(element);
 			res.send("Done");
 		}
+});
+
+//Lösche ein Element der ToDo Liste
+app.delete("/todo/:id",function(req,res){
+
+	var gId = parseInt(req.params.id,10);
+	var found = _.findWhere(todos,{id:gId});
+	
+	if(!found){
+		res.status(404).send();	
+	} else {
+		todos = _.without(todos,found);
+		res.json(found);
 	}
+	
 });
 
 
