78568de4d10f6f375dd18bb95f4c7df0e980e1d4
Patrick Meiler <patrick@meiler.org>
2018-03-01 21:58:12 +0100

    POST added

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
 const middleware = require("./middleware.js");
+const bodyParser = require("body-parser");
+
+
 
 var todos = [
 	{
@@ -21,27 +24,75 @@ var todos = [
 	}
 ]
 
+app.use(bodyParser.json());
 app.use(middleware.logger);
 
+
+
+
+
+/*
 app.get("/",function(req,res){
 	res.send("We listen!");
 });
+*/
 
+
+//Auflistung aller ToDo Elemente
 app.get("/todo",function(req,res){
 	res.json(todos);
 });
 
+
+
+//Ausgabe eines bestimmten ToDo Elementes
 app.get("/todo/:id",function(req,res){
-	var gId = req.params.id;
-	var found = false
+	var gId = parseInt(req.params.id,10);
+	var found;
+
 	todos.forEach(function(element){
-		if(gId == element.id){
-			found = true;
-			res.send("You have to do: " + element.description + " !");
+		if(gId === element.id){
+			found = element;
 		}
-	})
-	if(!found){res.status(404).send;}
+	});
+
+	if(found){
+		res.json(found);
+	}else{
+		res.status(404).send();
+	}
+	
 });
+
+app.post("/todo/add", function(req,res){
+	
+	/*
+	var a = req.body;
+	console.log(a);
+	console.log(a.id);
+	console.log(a.description);
+	res.send(req.body);
+	*/
+	addToArray(req.body);
+
+
+	function addToArray(element){
+		if(element.description == ""  || typeof element.description == "undefined"){
+			res.send("No Element was Provided!")
+		}else{
+
+			var newObject = {
+				element: todos.length,
+				description: element.description,
+				done: false
+			};
+			todos.push(newObject);
+			res.send("Done");
+		}
+	}
+});
+
+
 
 
 app.use(express.static(__dirname+"/public"));
